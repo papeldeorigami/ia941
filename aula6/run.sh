8c32c724e1d8a2eb858b253d0943e9d5edb339fe
Ricardo Andrade <papeldeorigami@googlemail.com>
2018-05-03 16:12:06 -0300

    aula6

--- aula6/run.sh
+++ aula6/run.sh
@@ -1,4 +1,5 @@
 java -jar ws3d-dist/WorldServer3D.jar &
 echo "Wait 3 seconds to initialize the virtual creature controller..."
 sleep 3
-java -jar DemoJSOAR/dist/DemoJSOAR.jar
+cd DemoJSOAR
+java -jar ./dist/DemoJSOAR.jar
